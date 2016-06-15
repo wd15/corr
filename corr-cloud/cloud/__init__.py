66b5c5b5b080061775083464e7e62d5360d563e1
Yannick Congo <faical.congo@nist.gov>
2016-06-15 11:28:43 -0400

    debugging file uploades.

--- corr-cloud/cloud/__init__.py
+++ corr-cloud/cloud/__init__.py
@@ -213,6 +213,8 @@ def s3_get_file(group='', key=''):
 
     try:
         res = obj.get()
+        print res
+        print res['Body']
         file_buffer.write(res['Body'].read())
         file_buffer.seek(0)
         return file_buffer
